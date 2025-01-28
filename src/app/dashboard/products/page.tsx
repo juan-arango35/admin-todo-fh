cd4fc6a3d329894085d845b6db93b4688ed01629
juan arango <juanarangozapata35@gmail.com>
2025-01-28 15:23:13 -0500

    agrega card de carrito de compras

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,13 +1,18 @@
-import { ProductCard } from '@/products/components/ProductCard'
-import React from 'react'
-
+import { ProductCard } from "@/products/components/ProductCard";
+import { products } from "@/products/data/products";
+import React from "react";
 
 const ProductsPage = () => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-3 gap-3'>
-    <ProductCard/>
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          {...product} //le pasamos todas las propiedades del producto
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
