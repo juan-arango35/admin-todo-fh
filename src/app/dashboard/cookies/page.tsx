1cd4770d80d58d61d682fb156287550c983a07d8
juan arango <juanarangozapata35@gmail.com>
2025-01-28 12:36:02 -0500

    agrega cookies

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,16 +1,22 @@
 import TabBar from "@/components/TabBar";
+import { cookies } from "next/headers";
 
 export const metadata = {
  title: 'Cookies',
  description: 'Cookies'
 };
 
-const CookiesPage = () => {
+const CookiesPage = async () => {
+  const cookiesStore = await cookies()
+  const cookieTab =  +(cookiesStore.get("selectedTab")?.value ?? "1")
+  const allCookies = cookiesStore.getAll()
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+      {JSON.stringify(allCookies, null, 2)}
       <div className="flex flex-col">
       <span className="text-3xl">Tabs</span>
-      <TabBar/>
+      <TabBar currentTab={cookieTab}/>
 
       </div>
     </div>
