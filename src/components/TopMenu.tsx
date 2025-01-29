cf807540fdbdcdb5bde949bc6aa9a4178cadfa87
juan arango <juanarangozapata35@gmail.com>
2025-01-29 12:05:31 -0500

    agrega navegacion a cart

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,4 +1,7 @@
+
 import { cookies } from "next/headers";
+import Link from "next/link";
+
 import {
   CiBellOn,
   CiChat1,
@@ -7,9 +10,7 @@ import {
   CiShoppingBasket,
 } from "react-icons/ci";
 
-
-
-const getTotalCount = (cart : {[id: string]: number}): number => {
+const getTotalCount = (cart: { [id: string]: number }): number => {
   let item = 0;
   Object.values(cart).forEach((value) => {
     item = item + (value as number);
@@ -20,10 +21,7 @@ const TopMenu = async () => {
   const cookieStore = await cookies();
   const cart = await JSON.parse(cookieStore.get("cart")?.value ?? "{}");
 
-
-  const totalItems = getTotalCount(cart)
-
-  
+  const totalItems = getTotalCount(cart);
 
   return (
     <div className="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
@@ -56,14 +54,20 @@ const TopMenu = async () => {
           <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
             <CiChat1 size={25} />
           </button>
-          <button className="flex  p-2 items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+
+          {/*     boton de carrito de compras */}
+          <Link
+            href="/dashboard/cart"
+            
+            className="flex  p-2 items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200"
+          >
             {totalItems > 0 && (
               <span className="text-sm mr-2 text-blue-900 font-bold">
                 {totalItems}
               </span>
             )}
             <CiShoppingBasket size={25} />
-          </button>
+          </Link>
         </div>
       </div>
     </div>
