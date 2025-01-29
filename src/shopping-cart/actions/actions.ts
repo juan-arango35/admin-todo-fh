9ed5beaccae82b0739ac4b10339c67504240cbfb
juan arango <juanarangozapata35@gmail.com>
2025-01-29 09:34:24 -0500

    agrega funcionalidad de boton de mas, para agregar ala carrito de compras

---
+++ src/shopping-cart/actions/actions.ts
@@ -0,0 +1,19 @@
+import { getCookie, hasCookie, setCookie } from "cookies-next";
+
+export const getCookieCart = (): { [id: string]: number } => {
+  if (hasCookie("cart")) {
+    const cookiecart = JSON.parse((getCookie("cart") as string) ?? "{}"); // busca la cookie
+    return cookiecart;
+  }
+  return {};
+};
+
+export const addProductToCart = (id: string) => {
+  const cookieCart = getCookieCart();
+  if (cookieCart[id]) {
+    cookieCart[id]++;
+  } else {
+    cookieCart[id] = 1;
+  }
+  setCookie("cart", JSON.stringify(cookieCart));
+};
