6337f8bbfbb77c43fd1372b728a97590471a740d
juan arango <juanarangozapata35@gmail.com>
2025-02-06 11:11:03 -0500

    agrega perfil de github desde el lado del servido

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   
   images: {
+    domains: ['avatars.githubusercontent.com'], 
     remotePatterns: [
       {
         hostname: "tailus.io",
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,9 @@ import Link from "next/link"
 import {  CiLogout } from "react-icons/ci"
 import SidebarItem from "./SidebarItem"
 import {  IoCalendarOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline, IoPrintOutline } from "react-icons/io5"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+
 
 
 
@@ -35,7 +38,16 @@ const menuItems =[
   }
 ]
 
-const Sidebar = () => {
+const Sidebar = async() => {
+
+//agregando la tarea
+  const session = await getServerSession(authOptions );
+ 
+
+
+  const name = session?.user?.name ?? "no Name"
+  const imageAvatar = session?.user?.image || "../../public/perfil.jpg"
+
   return (
      <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
            <div>
@@ -43,14 +55,14 @@ const Sidebar = () => {
                {/* TODO: Next/Link hacia dashboard */}
                <Link href="#" title="home">
                  {/* Next/Image */}
-                 <img src="/otro.webp" className="w-32"  alt="tailus logo"/>
+                 <img src="/otro.webp"  className="w-32"  alt="tailus logo"/>
                </Link>
              </div>
    
              <div className="mt-8 text-center">
                {/* Next/Image */}
-               <Image src="/perfil.jpg" alt="" className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28" width={50} height={50}/>
-                 <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Juan Arango</h5>
+               <Image src={imageAvatar}   alt="" className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28" width={50} height={50}/>
+                 <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{name}</h5>
                  <span className="hidden text-gray-400 lg:block">Admin</span>
              </div>
    
