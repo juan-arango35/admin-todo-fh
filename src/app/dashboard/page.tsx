a17e0d5e85bb95050496d782f624a233fcc768c4
juan arango <juanarangozapata35@gmail.com>
2025-02-06 09:47:11 -0500

    agrega acceso al dashboar desde github oaut

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,18 @@
+import WitgetItem from "@/components/WitgetItem";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
   return (
-    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-{/*       <WitgetItem /> */}
-
- 
+    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2">
+      <WitgetItem title="server-side USUARIO CONECTADO">
+        {JSON.stringify(session, null, 2)}
+      </WitgetItem>
     </div>
   );
 };
