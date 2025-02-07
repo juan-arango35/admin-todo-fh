ba939d2a1813d9028aa0746db8faa8a55e9fce24
juan arango <juanarangozapata35@gmail.com>
2025-02-06 19:07:23 -0500

    agrega modelo de usuarios de google y github

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,17 @@
+import { prisma } from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import { Adapter } from "next-auth/adapters";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
+
+
+
+
 export const authOptions = {
   // Configure one or more authentication providers
+  adapter: PrismaAdapter( prisma ) as Adapter,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
