ec988a681142bf336139da5643aa3dd62d13f545
juan arango <juanarangozapata35@gmail.com>
2025-02-12 17:45:33 -0500

    agrega seed de usuarios y agrega ralacion

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,22 +1,36 @@
 import { prisma } from '@/lib/prisma'
 
 import { NextResponse, NextRequest } from 'next/server'
+import bcrypt from 'bcryptjs';
 
 export async function POST(request: Request) { 
  await prisma.todo.deleteMany() //  delete * from todo
 
-/*     const todo = await prisma.todo.create({ data: { description: 'hola', complete: true }, })
-    console.log(todo, "todo")
- */
 
-    await prisma.todo.createMany({
-        data: [
-            { description: 'primera desripcion', complete: true },
-            { description: 'adios segunda', complete: false },
-            { description: 'adios tercera', complete: false },
-            { description: 'adios  cuarta', complete: false },
-            { description: 'adios  quinta', complete: true},
-        ]
+    await prisma.todo.deleteMany()
+    await prisma.user.deleteMany()
+
+    const user = await prisma.user.create({
+        data : {
+            email:"test1@google.com",
+            password: bcrypt.hashSync("123456"),
+            roles: ["admin", "client", "super-user"],
+            todos: {
+                create: [
+                    { description: 'primera desripcion', complete: true },
+                    { description: 'adios segunda', complete: false },
+                    { description: 'adios tercera', complete: false },
+                    { description: 'adios  cuarta', complete: false },
+                    { description: 'adios  quinta', complete: true},
+                ]
+            }
+        }
     })
+
+   /*  await prisma.todo.createMany({
+        data: [
+            
+        ]
+    }) */
  return NextResponse.json({ message: 'Soy la respuesta de seed 3' })
 }
